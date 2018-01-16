351c9769dd2587b2d52ee826208dac95803dde9a
包梦江 <baomengjiang@gamil.com>
2018-01-16 12:21:58 +0800

    fix(prowjob): fix command link to caicloud

--- prow/plugins/approve/approvers/owners.go
+++ prow/plugins/approve/approvers/owners.go
@@ -585,7 +585,7 @@ Associated issue requirement bypassed by: {{range $index, $approval := .ap.ListN
 
 {{ end -}}
 
-The full list of commands accepted by this bot can be found [here](https://go.k8s.io/bot-commands).
+The full list of commands accepted by this bot can be found [here](https://github.com/caicloud/engineering/blob/master/docs/caicloud_bot.md#bot-commands).
 
 <details {{if (and (not .ap.AreFilesApproved) (not (call .ap.ManuallyApproved))) }}open{{end}}>
 Needs approval from an approver in each of these OWNERS Files:
