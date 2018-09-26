69521f37954f057240b81d489bb6d45567ae554f
Mengjiang Bao <baomengjiang@gmail.com>
2018-09-26 18:47:36 +0800

    feat(label): add flow label command (#31)

--- prow/plugins/label/label.go
+++ prow/plugins/label/label.go
@@ -32,9 +32,9 @@ import (
 const pluginName = "label"
 
 var (
-	labelRegex              = regexp.MustCompile(`(?m)^/(area|committee|kind|priority|sig|branch|queue|version|scrum|status|from|rca|rc|rt|type|product|need|infra-area|resolution)\s*(.*)$`)
-	removeLabelRegex        = regexp.MustCompile(`(?m)^/remove-(area|committee|kind|priority|sig|branch|queue|version|scrum|status|from|rca|rc|rt|type|product|need|infra-area|resolution)\s*(.*)$`)
-	singleChoice            = flag.String("single-choice", "area,kind,priority,queue,scrum,status,from,rc,rt,type,product,resolution", "Comma separated list of command that needs support single-choice")
+	labelRegex              = regexp.MustCompile(`(?m)^/(area|committee|flow|kind|priority|sig|branch|queue|version|scrum|status|from|rca|rc|rt|type|product|need|infra-area|resolution)\s*(.*)$`)
+	removeLabelRegex        = regexp.MustCompile(`(?m)^/remove-(area|committee|flow|kind|priority|sig|branch|queue|version|scrum|status|from|rca|rc|rt|type|product|need|infra-area|resolution)\s*(.*)$`)
+	singleChoice            = flag.String("single-choice", "area,flow,kind,priority,queue,scrum,status,from,rc,rt,type,product,resolution", "Comma separated list of command that needs support single-choice")
 	nonExistentLabelOnIssue = "Those labels are not set on the issue: `%v`"
 )
 
